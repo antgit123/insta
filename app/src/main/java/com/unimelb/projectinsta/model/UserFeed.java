37954702443ba1c77249342ce776f5ba4d0c50fa
Tejal Jadhav <tjadhav@student.unimelb.edu.au>
2018-09-18 14:51:35 +1000

    Added UserPojo and added fields in UserFeed

--- app/src/main/java/com/unimelb/projectinsta/model/UserFeed.java
+++ app/src/main/java/com/unimelb/projectinsta/model/UserFeed.java
@@ -1,8 +1,20 @@
 package com.unimelb.projectinsta.model;
 
-public class UserFeed {
+import android.location.Location;
 
-    //will be replaced with User model class
+import java.util.Date;
+import java.util.List;
+
+public class UserFeed {
+    private UserPojo user;
+    private String photo;
+    private Location location;
+    private String hashtag;
+    private Date date;
+    private List<Like> likeList;
+    private List<Comment> commentList;
+
+
     private String m_UserName;
     private String m_Location;
     private int m_Img;
@@ -13,6 +25,17 @@ public class UserFeed {
         this.m_Location = location;
         this.m_Img = image_resource;
         this.m_Description = description;
+    }
+
+    public UserFeed(UserPojo user, String photo, Location location, String hashtag, Date date, List<Like> likeList, List<Comment> commentList, String m_Description) {
+        this.user = user;
+        this.photo = photo;
+        this.location = location;
+        this.hashtag = hashtag;
+        this.date = date;
+        this.likeList = likeList;
+        this.commentList = commentList;
+        this.m_Description = m_Description;
     }
 
     public String getM_UserName() {
@@ -45,6 +68,62 @@ public class UserFeed {
 
     public void setM_Location(String m_Location) {
         this.m_Location = m_Location;
+    }
+
+    public UserPojo getUser() {
+        return user;
+    }
+
+    public void setUser(UserPojo user) {
+        this.user = user;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getHashtag() {
+        return hashtag;
+    }
+
+    public void setHashtag(String hashtag) {
+        this.hashtag = hashtag;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<Like> getLikeList() {
+        return likeList;
+    }
+
+    public void setLikeList(List<Like> likeList) {
+        this.likeList = likeList;
+    }
+
+    public List<Comment> getCommentList() {
+        return commentList;
+    }
+
+    public void setCommentList(List<Comment> commentList) {
+        this.commentList = commentList;
     }
 }
 
