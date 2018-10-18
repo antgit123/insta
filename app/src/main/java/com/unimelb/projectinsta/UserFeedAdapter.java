9882709d9cb52a1001f65b283fe9447714e8f550
antgit123 <antariksh.maverick92@gmail.com>
2018-10-18 22:19:35 +1100

    Heart color change function

--- app/src/main/java/com/unimelb/projectinsta/UserFeedAdapter.java
+++ app/src/main/java/com/unimelb/projectinsta/UserFeedAdapter.java
@@ -1,5 +1,7 @@
 package com.unimelb.projectinsta;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -9,6 +11,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
+
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -22,6 +28,9 @@ public class UserFeedAdapter extends RecyclerView.Adapter<UserFeedHolder> {
 
     ArrayList<UserFeed> userFeed;
     Context userFeedContext;
+    ItemClickListener listener;
+    DecelerateInterpolator decelerate = new DecelerateInterpolator();
+    AccelerateInterpolator accelerate = new AccelerateInterpolator();
 
     public UserFeedAdapter(Context userFeedContext, ArrayList<UserFeed> userFeed){
         this.userFeedContext = userFeedContext;
@@ -37,6 +46,7 @@ public class UserFeedAdapter extends RecyclerView.Adapter<UserFeedHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull final UserFeedHolder userFeedHolder, int i) {
+
         userFeedHolder.userName_txt.setText(userFeed.get(i).getM_UserName());
         userFeedHolder.description_txt.setText(userFeed.get(i).getM_Description());
         userFeedHolder.title.setText(userFeed.get(i).getM_Location());
@@ -47,8 +57,6 @@ public class UserFeedAdapter extends RecyclerView.Adapter<UserFeedHolder> {
             @Override
             public void onSuccess(Uri uri) {
                 Log.i("URI:",uri.toString());
-//                userFeedHolder.img.setImageURI(uri);
-
                 Glide.with(userFeedContext).load(uri.toString()).into(userFeedHolder.img);
             }
         }).addOnFailureListener(new OnFailureListener() {
@@ -61,15 +69,46 @@ public class UserFeedAdapter extends RecyclerView.Adapter<UserFeedHolder> {
         userFeedHolder.setItemClickListener(new ItemClickListener() {
             @Override
             public void onItemClick(View v, int pos) {
-                Snackbar.make(v,userFeed.get(pos).getM_UserName(),Snackbar.LENGTH_SHORT).show();
+                if(v.getId() == userFeedHolder.white_heart_icon.getId()){
+                    toggleLike(pos,false,userFeedHolder);
+                }
+                if(v.getId() == userFeedHolder.red_heart_icon.getId()){
+                    toggleLike(pos,true,userFeedHolder);
+                }
             }
         });
-        userFeedHolder.setItemClickListener(new ItemClickListener() {
-            @Override
-            public void onItemClick(View v, int pos) {
-                Snackbar.make(v,userFeed.get(pos).getM_UserName(),Snackbar.LENGTH_SHORT).show();
-            }
-        });
+    }
+
+    public void toggleLike(int position,boolean isLiked,UserFeedHolder userFeedHolder){
+        AnimatorSet animatorSet = new AnimatorSet();
+        if(isLiked){
+            userFeedHolder.red_heart_icon.setScaleX(0.1f);
+            userFeedHolder.red_heart_icon.setScaleY(0.1f);
+
+            ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(userFeedHolder.red_heart_icon,"scaleY",1f,0f);
+            scaleDownY.setDuration(300);
+            scaleDownY.setInterpolator(accelerate);
+            ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(userFeedHolder.red_heart_icon,"scaleX",1f,0f);
+            scaleDownX.setDuration(300);
+            scaleDownX.setInterpolator(accelerate);
+            userFeedHolder.red_heart_icon.setVisibility(View.INVISIBLE);
+            userFeedHolder.white_heart_icon.setVisibility(View.VISIBLE);
+            animatorSet.playTogether(scaleDownY,scaleDownX);
+        }else{
+            userFeedHolder.red_heart_icon.setScaleX(0.1f);
+            userFeedHolder.red_heart_icon.setScaleY(0.1f);
+
+            ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(userFeedHolder.red_heart_icon,"scaleY",0.1f,1f);
+            scaleDownY.setDuration(300);
+            scaleDownY.setInterpolator(decelerate);
+            ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(userFeedHolder.red_heart_icon,"scaleX",0.1f,1f);
+            scaleDownX.setDuration(300);
+            scaleDownX.setInterpolator(decelerate);
+            userFeedHolder.red_heart_icon.setVisibility(View.VISIBLE);
+            userFeedHolder.white_heart_icon.setVisibility(View.INVISIBLE);
+            animatorSet.playTogether(scaleDownY,scaleDownX);
+        }
+        animatorSet.start();
     }
 
     @Override
