48e565a769e0a6728f81f57ff8f2a8e698cd8a5c
Tejal Jadhav <tjadhav@student.unimelb.edu.au>
2018-10-19 15:23:31 +1100

    changes to userfeed pojo

--- app/src/main/java/com/unimelb/projectinsta/model/UserFeed.java
+++ app/src/main/java/com/unimelb/projectinsta/model/UserFeed.java
@@ -8,72 +8,28 @@ import java.util.List;
 public class UserFeed {
     private UserPojo user;
     private String photo;
-    private Location location;
-    private String hashtag;
+    private String location;
+    private String caption;
     private Date date;
     private List<Like> likeList;
     private List<Comment> commentList;
     private int feed_Id;
     private int comments;
 
-
-    private String m_UserName;
-    private String m_Location;
-    private int m_Img;
-    private String m_Description;
-
     public UserFeed(){
 
     }
 
-    public UserFeed(String userName, String location, int image_resource, String description){
-        this.m_UserName = userName;
-        this.m_Location = location;
-        this.m_Img = image_resource;
-        this.m_Description = description;
-    }
-
-    public UserFeed(UserPojo user, String photo, Location location, String hashtag, Date date, List<Like> likeList, List<Comment> commentList, String m_Description) {
+    public UserFeed(UserPojo user, String photo, String location, String caption, Date date, List<Like> likeList, List<Comment> commentList, int feed_Id, int comments) {
         this.user = user;
         this.photo = photo;
         this.location = location;
-        this.hashtag = hashtag;
+        this.caption = caption;
         this.date = date;
         this.likeList = likeList;
         this.commentList = commentList;
-        this.m_Description = m_Description;
-    }
-
-    public String getM_UserName() {
-        return m_UserName;
-    }
-
-    public void setM_UserName(String m_UserName) {
-        this.m_UserName = m_UserName;
-    }
-
-    public int getM_Img() {
-        return m_Img;
-    }
-
-    public void setM_Img(int m_Img) {
-        this.m_Img = m_Img;
-    }
-
-    public String getM_Description() {
-        return m_Description;
-    }
-
-    public void setM_Description(String m_Description) {
-        this.m_Description = m_Description;
-    }
-
-    public String getM_Location() {
-        return m_Location;
-    }
-
-    public void setM_Location(String m_Location) {
-        this.m_Location = m_Location;
+        this.feed_Id = feed_Id;
+        this.comments = comments;
     }
 
     public UserPojo getUser() {
@@ -92,20 +48,20 @@ public class UserFeed {
         this.photo = photo;
     }
 
-    public Location getLocation() {
+    public String getLocation() {
         return location;
     }
 
-    public void setLocation(Location location) {
+    public void setLocation(String location) {
         this.location = location;
     }
 
-    public String getHashtag() {
-        return hashtag;
+    public String getCaption() {
+        return caption;
     }
 
-    public void setHashtag(String hashtag) {
-        this.hashtag = hashtag;
+    public void setCaption(String caption) {
+        this.caption = caption;
     }
 
     public Date getDate() {
@@ -130,6 +86,37 @@ public class UserFeed {
 
     public void setCommentList(List<Comment> commentList) {
         this.commentList = commentList;
+    }
+
+    public int getFeed_Id() {
+        return feed_Id;
+    }
+
+    public void setFeed_Id(int feed_Id) {
+        this.feed_Id = feed_Id;
+    }
+
+    public int getComments() {
+        return comments;
+    }
+
+    public void setComments(int comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public String toString() {
+        return "UserFeed{" +
+                "user=" + user +
+                ", photo='" + photo + '\'' +
+                ", location='" + location + '\'' +
+                ", caption='" + caption + '\'' +
+                ", date=" + date +
+                ", likeList=" + likeList +
+                ", commentList=" + commentList +
+                ", feed_Id=" + feed_Id +
+                ", comments=" + comments +
+                '}';
     }
 }
 
