2133ade740e7f50a44086c417cb8374d6dfaa8bb
Tejal Jadhav <tjadhav@student.unimelb.edu.au>
2018-10-20 15:13:26 +1100

    changes for Bluetooth

--- app/src/main/java/com/unimelb/projectinsta/HomeFragment.java
+++ app/src/main/java/com/unimelb/projectinsta/HomeFragment.java
@@ -1,10 +1,20 @@
 package com.unimelb.projectinsta;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -33,9 +43,14 @@ import com.unimelb.projectinsta.model.UserFeed;
 import com.unimelb.projectinsta.model.UserPojo;
 import com.unimelb.projectinsta.util.CommonUtil;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.UUID;
 
 
 /**
@@ -64,6 +79,44 @@ public class HomeFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private static final String TAG = "haha";
+
+    private static final int REQUEST_BLUETOOTH_PERMISSIONS = 1;
+
+    private static final int REQUEST_ENABLE_BT = 2;
+
+    BluetoothAdapter bluetoothAdapter;
+
+    private static UUID MY_UUID = null;
+
+    private ServerThread serverThread;
+
+    private volatile boolean isServerRunning = false;
+
+    private static final int MESSAGE_RECEIVED = 0;
+    private static final int CONNECTION_SUCCESSFUL = 1;
+
+    InputStream ServerInStream = null;
+    OutputStream ServerOutStream = null;
+
+    private static android.os.Handler handler_process = new android.os.Handler(){
+        public void handleMessage(Message msg){
+            if (msg.what==MESSAGE_RECEIVED){
+                Log.d(TAG, msg.obj.toString());
+            }else if (msg.what==CONNECTION_SUCCESSFUL){
+                Log.d(TAG, "CONNECTION_SUCCESSFUL");
+            }
+        }
+    };
+
+    private IntentFilter filter;
+
+    private static final String[] BLUE_PERMISSIONS = {
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
 
     public HomeFragment() {
         // Required empty public constructor
@@ -94,6 +147,8 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        MY_UUID = new DeviceUuidFactory(getContext()).getDeviceUuid();
+        startBluetoothSensor();
     }
 
     @Override
@@ -172,7 +227,7 @@ public class HomeFragment extends Fragment {
        });
     }
 
-    private void getFeeds() {
+    public void getFeeds() {
         if (loggedInUser.getFollowingList().size() > 0) {
             List<String> followingUserIds = loggedInUser.getFollowingList();
             //query feeds
@@ -198,6 +253,291 @@ public class HomeFragment extends Fragment {
                 });
             }
         }
+    }
+
+    public void startBluetoothSensor(){
+
+        // This only targets API 23+
+        // check permission using a thousand lines (Google is naive!)
+
+
+        if (!hasPermissionsGranted(BLUE_PERMISSIONS)) {
+            requestBluePermissions(BLUE_PERMISSIONS);
+            return;
+        }
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (bluetoothAdapter == null)
+        {
+            Log.d(TAG, "Device has no bluetooth");
+            return;
+        }
+
+        // ask users to open bluetooth
+        if (bluetoothAdapter.isEnabled()==false){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+
+        // make this device visible to others for 3000 seconds
+        Intent discoverableIntent = new
+                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 3000);
+        startActivity(discoverableIntent);
+
+        // start server
+        becomeServer();
+    }
+
+
+    // be a server
+
+    public void becomeServer() {
+
+        Log.d(TAG, "152");
+        serverThread = new ServerThread();
+        serverThread.start();
+    }
+
+    private class ServerThread extends Thread{
+        private final BluetoothServerSocket serverSocket;
+
+        private ServerThread() {
+            BluetoothServerSocket tmp = null;
+            try {
+                tmp = bluetoothAdapter.listenUsingRfcommWithServiceRecord("myServer", MY_UUID);
+                Log.d(TAG, "@ UUID "+MY_UUID);
+            } catch (IOException e) {
+                Log.d(TAG, "Server establishing failed");
+            }
+            serverSocket = tmp;
+        }
+
+        public void sendData(String data) {
+            StringBuffer sb = new StringBuffer();
+            sb.append(data);
+            sb.append("\n");
+            if (ServerOutStream != null) {
+                try {
+                    ServerOutStream.write(sb.toString().getBytes());
+                    ServerOutStream.flush();
+                    Log.d(TAG,"@ send data " + sb.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.d(TAG,"@ Server sending fail");
+                }
+            }
+        }
+
+        public void closeServer(){
+            this.interrupt();
+        }
+
+
+        public void run() {
+            Log.d(TAG, "start server.");
+            BluetoothSocket connectSocket = null;
+            String line = "";
+
+            isServerRunning = true;
+
+            // Keep listening until exception occurs or a socket is returned.
+            while (isServerRunning) {
+                try {
+                    Log.d(TAG, "waiting for connection");
+                    connectSocket = serverSocket.accept();
+                } catch (IOException e) {
+                    Log.d(TAG, "Socket's accept() method failed", e);
+                    break;
+                }
+
+                if (connectSocket != null) {
+                    // A connection was accepted. Perform work later.
+                    break;
+                }
+
+                if(Thread.currentThread().isInterrupted())
+                {
+
+                    try {
+                        Log.d(TAG, "quit during waiting for connection");
+                        serverSocket.close();
+                    } catch (IOException e) {
+                        Log.d(TAG, "error quit during waiting", e);
+                        break;
+                    }
+
+                    isServerRunning = false;
+                    break;
+                }
+            }
+
+            while (isServerRunning) {
+                try {
+                    Log.d(TAG, "do work with connection");
+                    serverSocket.close();
+
+                    if (connectSocket != null) {
+                        // A connection was accepted. Perform work here
+
+                        bluetoothAdapter.cancelDiscovery();
+                        Log.d(TAG, "@ Connection Successfully");
+                        // send a message to the UI thread
+                        Message message = new Message();
+                        message.what=CONNECTION_SUCCESSFUL;
+                        handler_process.sendMessage(message);
+                    }
+                    else {
+                        isServerRunning = false;
+                        break;
+                    }
+
+                } catch (IOException e) {
+                    Log.d(TAG, "Connection failed");
+                    break;
+                }
+                try {
+                    Log.d(TAG, "@ line1"+line);
+
+                    if(ServerInStream == null) {
+                        InputStream tmpIn = null;
+                        // Get the input and output streams; using temp objects because
+                        // member streams are final.
+                        try {
+                            tmpIn = connectSocket.getInputStream();
+                        } catch (Exception e) {
+                            Log.e(TAG, "Error occurred when creating input stream", e);
+                        }
+                        ServerInStream = tmpIn;
+                    }
+
+                    if(ServerOutStream == null) {
+                        OutputStream tmpOut = null;
+                        // Get the input and output streams; using temp objects because
+                        // member streams are final.
+                        try {
+                            tmpOut = connectSocket.getOutputStream();
+                        } catch (Exception e) {
+                            Log.e(TAG, "Error occurred when creating output stream", e);
+                        }
+                        ServerOutStream = tmpOut;
+                    }
+
+
+                    // send data to client
+                    sendData("Server: Hello!");
+
+                    Log.d(TAG, "254");
+
+                    while (isServerRunning &&ServerInStream != null) {
+
+                        BufferedReader br = new BufferedReader(new InputStreamReader(ServerInStream));
+                        // readLine() read and delete one line
+                        while ((line = br.readLine()) != null) {
+                            Log.d(TAG, "@message " + line);
+                            // send a message to the UI thread
+                            Message message = new Message();
+                            message.what = MESSAGE_RECEIVED;
+                            message.obj = line;
+                            handler_process.sendMessage(message);
+
+                        }
+
+                        // send data to client
+                        sendData("Server: Hello!");
+
+                        if(Thread.currentThread().isInterrupted())
+                        {
+                            connectSocket.close();
+                            Log.d(TAG, "quit from connection");
+                            isServerRunning = false;
+                            break;
+                        }
+
+                    }
+
+                    if(Thread.currentThread().isInterrupted())
+                    {
+                        connectSocket.close();
+                        Log.d(TAG, "quit from connection");
+                        isServerRunning = false;
+                        break;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+
+                    Log.d(TAG, "quit from connection");
+
+                    try {
+                        connectSocket.close();
+                    } catch (Exception e2) {
+                        Log.e(TAG, "Error occurred in an exception of an exception", e2);
+                    }
+                    isServerRunning = false;
+
+                    break;
+                }
+
+            }
+
+        }
+
+    }
+
+
+
+    // check if app has a list of permissions, then request not-granted ones
+
+    public void requestBluePermissions(String[] permissions) {
+        Log.d(TAG, "line 376");
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(getContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(permission);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(getActivity(), listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_BLUETOOTH_PERMISSIONS);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_BLUETOOTH_PERMISSIONS:
+                Log.d(TAG, "line 394");
+
+                if (grantResults.length > 0) {
+                    for (int grantResult : grantResults) {
+                        if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                            Log.d("haha", "one or more permission denied");
+                            return;
+                        }
+                    }
+                    Log.d("haha", "all permissions granted");
+
+                }
+
+        }
+    }
+
+
+    private boolean hasPermissionsGranted(String[] permissions) {
+        Log.d(TAG, "411");
+        for (String permission : permissions) {
+            if (ActivityCompat.checkSelfPermission(getContext(), permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
--- app/src/main/java/com/unimelb/projectinsta/profile/EnlargedPostViewFragment.java
+++ app/src/main/java/com/unimelb/projectinsta/profile/EnlargedPostViewFragment.java
@@ -1,12 +1,25 @@
 package com.unimelb.projectinsta.profile;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,15 +28,24 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.unimelb.projectinsta.DeviceUuidFactory;
 import com.unimelb.projectinsta.OnSwipeTouchListener;
 import com.unimelb.projectinsta.R;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -44,6 +66,44 @@ public class EnlargedPostViewFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    private static final String TAG = "haha";
+
+    private static final int REQUEST_BLUETOOTH_PERMISSIONS = 1;
+
+    private static final int REQUEST_ENABLE_BT = 2;
+
+    BluetoothAdapter bluetoothAdapter;
+
+    private static UUID MY_UUID = null;
+
+    private ClientThread clientThread;
+
+    private volatile boolean isClientRunning = false;
+
+    private static final int MESSAGE_RECEIVED = 0;
+    private static final int CONNECTION_SUCCESSFUL = 1;
+
+    InputStream ClientInStream = null;
+    OutputStream ClientOutStream = null;
+
+    private static android.os.Handler handler_process = new android.os.Handler(){
+        public void handleMessage(Message msg){
+            if (msg.what==MESSAGE_RECEIVED){
+                Log.d(TAG, msg.obj.toString());
+            }else if (msg.what==CONNECTION_SUCCESSFUL){
+                Log.d(TAG, "CONNECTION_SUCCESSFUL");
+            }
+        }
+    };
+
+    private IntentFilter filter;
+
+    private static final String[] BLUE_PERMISSIONS = {
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
 
     public EnlargedPostViewFragment() {
         // Required empty public constructor
@@ -74,6 +134,7 @@ public class EnlargedPostViewFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        MY_UUID = new DeviceUuidFactory(getContext()).getDeviceUuid();
     }
 
     @Override
@@ -99,6 +160,7 @@ public class EnlargedPostViewFragment extends Fragment {
             @Override
             public void onSwipeRight() {
                 Toast.makeText(getActivity(), "swipe right!!!", Toast.LENGTH_SHORT).show();
+                startBluetoothSensor();
             }
         });
         return view;
@@ -142,4 +204,287 @@ public class EnlargedPostViewFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    public void startBluetoothSensor(){
+
+        // This only targets API 23+
+        // check permission using a thousand lines (Google is naive!)
+
+        if (!hasPermissionsGranted(BLUE_PERMISSIONS)) {
+            requestBluePermissions(BLUE_PERMISSIONS);
+            return;
+        }
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (bluetoothAdapter == null)
+        {
+            Log.d(TAG, "Device has no bluetooth");
+            return;
+        }
+
+        // ask users to open bluetooth
+        if (bluetoothAdapter.isEnabled()==false){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+
+
+        //to start scanning whether there are any other Bluetooth devices
+        bluetoothAdapter.startDiscovery();
+
+        //register the BroadcastReceiver to broadcast discovered devices
+        filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        //registerReceiver(receiver, filter);
+
+        //return paired devices
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                Log.d(TAG, "@ paired devices: "+device.getAddress());
+            }
+        }
+    }
+
+    //Create a BroadcastRecevier for ACTION_FOUND
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            //when discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)){
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Log.d(TAG, "@ discovered devices: "+device.getAddress());
+
+                // call becomeClient
+                // use your server device Bluetooth address
+                //
+                if (device.getAddress().equals("A8:0C:63:4D:6F:D6"))
+                {
+                    becomeClient(device);
+                }
+
+            }
+        }
+    };
+
+    // be a client
+
+    public void becomeClient(BluetoothDevice device) {
+        clientThread = new ClientThread(device);
+        clientThread.start();
+    }
+
+    private class ClientThread extends Thread{
+
+        private final BluetoothSocket clientSocket;
+        private final BluetoothDevice clientDevice;
+
+        private ClientThread(BluetoothDevice device) {
+            BluetoothSocket tmp = null;
+            try {
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+            clientDevice = device;
+            clientSocket = tmp;
+        }
+
+        public void sendData(String data) {
+            StringBuffer sb = new StringBuffer();
+            sb.append(data);
+            sb.append("\n");
+            if (ClientOutStream != null) {
+                try {
+                    ClientOutStream.write(sb.toString().getBytes());
+                    ClientOutStream.flush();
+                    Log.d(TAG,"@ send data " + sb.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.d(TAG,"@ Client sending fail");
+                }
+            }
+        }
+
+        public void closeClient(){
+            this.interrupt();
+        }
+
+
+        public void run() {
+            Log.d(TAG, "start client.");
+
+            isClientRunning = true;
+
+            bluetoothAdapter.cancelDiscovery();
+
+            String line = "";
+
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                clientSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    clientSocket.close();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
+
+            // do work with connection
+
+            while (isClientRunning) {
+
+                Log.d(TAG, "do work with connection");
+
+                try {
+                    Log.d(TAG, "@ line1"+line);
+
+                    if(ClientInStream == null) {
+                        InputStream tmpIn = null;
+                        // Get the input and output streams; using temp objects because
+                        // member streams are final.
+                        try {
+                            tmpIn = clientSocket.getInputStream();
+                        } catch (Exception e) {
+                            Log.e(TAG, "Error occurred when creating input stream", e);
+                        }
+                        ClientInStream = tmpIn;
+                    }
+
+                    if(ClientOutStream == null) {
+                        OutputStream tmpOut = null;
+                        // Get the input and output streams; using temp objects because
+                        // member streams are final.
+                        try {
+                            tmpOut = clientSocket.getOutputStream();
+                        } catch (Exception e) {
+                            Log.e(TAG, "Error occurred when creating output stream", e);
+                        }
+                        ClientOutStream = tmpOut;
+                    }
+
+                    // send data to server
+                    sendData("Client: Hello!");
+
+                    Log.d(TAG, "254");
+
+                    // listen to server
+                    while (isClientRunning &&ClientInStream != null) {
+
+                        Log.d(TAG, "257");
+
+                        BufferedReader br = new BufferedReader(new InputStreamReader(ClientInStream));
+                        // readLine() read and delete one line
+                        while ((line = br.readLine()) != null) {
+                            Log.d(TAG, "@message " + line);
+                            // send a message to the UI thread
+                            Message message = new Message();
+                            message.what = MESSAGE_RECEIVED;
+                            message.obj = line;
+                            handler_process.sendMessage(message);
+
+                        }
+
+
+                        if(Thread.currentThread().isInterrupted())
+                        {
+                            clientSocket.close();
+                            Log.d(TAG, "quit from connection");
+                            isClientRunning = false;
+                            break;
+                        }
+
+                    }
+
+                    if(Thread.currentThread().isInterrupted())
+                    {
+                        clientSocket.close();
+                        Log.d(TAG, "quit from connection");
+                        isClientRunning = false;
+                        break;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.d(TAG, "@ exception");
+
+                    Log.d(TAG, "quit from connection");
+
+                    try {
+                        clientSocket.close();
+                    } catch (Exception e2) {
+                        Log.e(TAG, "Error occurred in an exception of an exception", e2);
+                    }
+                    isClientRunning = false;
+
+                    break;
+                }
+
+            }
+
+        }
+
+    }
+
+
+
+    // check if app has a list of permissions, then request not-granted ones
+
+    public void requestBluePermissions(String[] permissions) {
+        Log.d(TAG, "line 376");
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(getContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(permission);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(getActivity(), listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_BLUETOOTH_PERMISSIONS);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_BLUETOOTH_PERMISSIONS:
+                Log.d(TAG, "line 394");
+
+                if (grantResults.length > 0) {
+                    for (int grantResult : grantResults) {
+                        if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                            Log.d("haha", "one or more permission denied");
+                            return;
+                        }
+                    }
+                    Log.d("haha", "all permissions granted");
+
+                }
+
+        }
+    }
+
+
+    private boolean hasPermissionsGranted(String[] permissions) {
+        Log.d(TAG, "411");
+        for (String permission : permissions) {
+            if (ActivityCompat.checkSelfPermission(getContext(), permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
